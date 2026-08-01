8fc8fd5c995b9532c9e7e4f72e49d12d4b588c48
Vishal Verma <vishal098verma@gmail.com>
2026-08-02 01:38:36 +0530

    Add solution v1 for Concatenate Non-Zero Digits and Multiply by Sum I (java)

---
+++ concatenate-non-zero-digits-and-multiply-by-sum-i/solutions/solution_1_2026-08-02.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long sumAndMultiply(int n) {
+        long x=0;
+        int sum=0;
+        String number= String.valueOf(n);
+
+        for (char ch: number.toCharArray()){
+            int digit=ch-'0';
+            if (digit!=0){
+                x=x*10+digit;
+                sum+=digit;
+            }
+        }
+        return x*sum;
+    }
+}
